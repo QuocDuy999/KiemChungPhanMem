3dc296cb326168b9e1a2e3ee43db73f97cf94bf4
Quocduy999 <quocduy74525@gmail.com>
2026-05-27 01:41:16 +0700

    update code

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,6 +1,14 @@
-ALTER DATABASE foodstore CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE food_store_db
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+USE food_store_db;
+
 CREATE TABLE IF NOT EXISTS roles (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   description TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci;
