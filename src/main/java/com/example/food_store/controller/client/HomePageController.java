879846e8b6597c9ed68977d45f502c840c579b69
Dkhanh95 <dinhduykhanh170398@gmail.com>
2026-06-30 21:53:11 +0700

    KAN-32 Fix Lỗi File HomePageController.java: A NullPointerException could be thrown; getFieldError() can return null.

--- src/main/java/com/example/food_store/controller/client/HomePageController.java
+++ src/main/java/com/example/food_store/controller/client/HomePageController.java
@@ -208,7 +208,13 @@ public class HomePageController extends BaseController {
             BindingResult bindingResult, Model model) {
         log.info("Request to /change-password");
         if (bindingResult.hasErrors()) {
-            String error = bindingResult.getFieldError().getDefaultMessage();
+            String error = "Dữ liệu không hợp lệ";
+
+            FieldError fieldError = bindingResult.getFieldError();
+            if (fieldError != null) {
+                error = fieldError.getDefaultMessage();
+            }
+
             model.addAttribute("errorNewpassword", error);
             return "client/homepage/changePassword";
         }
